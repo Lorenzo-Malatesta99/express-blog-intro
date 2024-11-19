7be98f79e05cc6ab1b5e289a149d6ef0651a8712
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-11-19 17:05:21 +0100

    ADD esercizio

---
+++ post-1.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    title: "Ciambellone",
+    content:
+      "Il Ciambellone è una torta soffice e deliziosa, perfetta sia per la colazione che per la merenda. È un dolce semplice ma gustoso, arricchito con aroma fresco di limone.",
+    image: "img/ciambellone.jpeg",
+  },
+  {
+    title: "Cracker di Barbabietola",
+    content:
+      "I cracker di barbabietola sono croccanti e salutari, realizzati con la dolcezza naturale della barbabietola. Perfetti come snack leggero e ricco di nutrienti.",
+    image: "img/cracker_barbabietola.jpeg",
+  },
+  {
+    title: "Pane Dolce Fritto",
+    content:
+      "Il pane fritto dolce è una golosità semplice, preparato con fette di pane dorate e croccanti, ricoperte di zucchero e cannella. Ideale per una merenda sfiziosa.",
+    image: "img/pane_fritto_dolce.jpeg",
+  },
+  {
+    title: "Pasta alla Barbabietola",
+    content:
+      "La pasta alla barbabietola è un piatto colorato e nutriente, con una salsa cremosa a base di barbabietola. Un perfetta alternativa sana alla pasta tradizionale.",
+    image: "img/pasta_barbabietola.jpeg",
+  },
+  {
+    title: "Dolce Paesano",
+    content:
+      "Il dolce paesano è un dessert rustico e ricco di sapori, preparato con ingredienti semplici come pane raffermo, cacao e noci. Un dolce tipico che evoca i sapori della tradizione contadina.",
+    image: "img/torta_paesana.jpeg",
+  },
+];
+
+module.exports = posts;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const posts = require("./posts");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  res.json({
+    count: posts.length,
+    posts: posts,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server attivo su http://localhost:${port}`);
+});
